9b851eb5200cb592c10ca32597f2efa2d4f888ac
Stefan Rein <mail@stefanrein.de>
2017-06-01 12:49:54 +0200

    Update app.module.ts
    Importing this one here is not correct

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { MeetModule } from './meet/meet.module';
 
 @NgModule({
     declarations: [
         AppComponent
     ],
     imports: [
-        MeetModule,
         BrowserModule,
         AppRoutingModule
     ],
